5f9571c494cd1d122e6ca695e0ffb913324e86e3
Jordi <jordi.hermoso.up@gmail.com>
2020-10-15 19:24:04 +0800

    rn-navigation v4 dependencies added, font loading added to app.tsx, initial screens etc.

--- rn-meals-app/App.tsx
+++ rn-meals-app/App.tsx
@@ -1,23 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const fetchFonts = () => {
+	return Font.loadAsync({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	});
+};
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+export default function App() {
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
+	const [dataLoaded, setDataLoaded] = useState(false);
+
+	if (!dataLoaded) {
+		// Expo component that knows when async prop finishes
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setDataLoaded(true)}
+				onError={() => console.log('error loading fonts')}
+			/>
+		);
+	}
+
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+				<Navigation colorScheme={colorScheme} />
+				<StatusBar />
+			</SafeAreaProvider>
+		);
+	}
 }
